52801083c57f294d24e6f8d3250a5d440bc5ee4e
kiren77 <kiren77@users.noreply.github.com>
2021-01-08 19:28:50 +1100

    yup sdf

--- VBGAMES/Playground/playground.js
+++ VBGAMES/Playground/playground.js
@@ -1,4 +1,61 @@
-if ('WebSocket' in window) { (function () { function refreshCSS() 
+//nested array
+//19 pots of paint
+//4 red
+//4 blue
+//4 yellow
+//3 orange
+//3 green
+//1 purple (desert)
+
+//instead of island tiles let's use pots of paint
+
+let sheepTile;
+let woodTile;
+let wheatTile;
+let oreTile;
+let brickTile;
+let desertTile;
+
+
+const remainingTiles = [
+    {sheepTile: '#008000',  amountTiles: 4},//sheep
+    {woodTile:  '#bdc001',  amountTiles: 4},//wood
+    {wheatTile: '#606113',  amountTiles: 4},//wheat
+    {oreTile:   '#bbbba3',  amountTiles: 3},//ore
+    {brickTile: '#670320',  amountTiles: 3},//brick
+    {desertTile:'#606113',  amountTiles: 1}//desert
+]
+
+
+//if there are items left in the
+const tilesLeft = remainingTiles.some((tile) => {
+   return tile.amountTiles > 0
+})
+// if there are tiles left the console returns true
+console.log(tilesLeft);
+
+//we want to distribute all Tiles
+
+//
+/*const tilesLeft = remainingTiles.some((tile) => {
+   return tile.amountTiles > 0
+})
+// if there are tiles left the console returns true
+console.log(tilesLeft);
+*/
+
+
+
+
+
+
+function colorFunction() {
+    //document.body.style.backgroundColor = "red"
+    //(we replace .body with .querySelector("#cellOne"))
+    document.querySelector("td").style.backgroundColor = "blue";
+}
+
+/*if ('WebSocket' in window) { (function () { function refreshCSS() 
     { var sheets = [].slice.call(document.getElementsByTagName("link")); 
     var head = document.getElementsByTagName("head")[0]; 
     for (var i = 0; i < sheets.length; ++i) 
@@ -16,4 +73,4 @@ if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServ
 sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true); 
 } })();
  } else { console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.'); 
-}
+}*/
