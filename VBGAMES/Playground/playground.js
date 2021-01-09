67231a1755b573e333f9152799ba972d53948a95
kiren77 <kiren77@users.noreply.github.com>
2021-01-10 03:52:46 +1100

    commit

--- VBGAMES/Playground/playground.js
+++ VBGAMES/Playground/playground.js
@@ -8,7 +8,6 @@
 //1 purple (desert)
 
 
-
 let sheepTile, woodTile, wheatTile, oreTile, brickTile, desertTile;
 const tileType = ["sheep", "wood", "wheat", "ore", "brick", "desert"];
 
@@ -23,46 +22,78 @@ const remainingTiles = [
     {desertTile:'#606113',  amountTiles: 1}//desert
 ];
 
-//console.log(`color of ${remainingTiles[0]}  ${remainingTiles[0].sheepTile}`);
 
 //all cells
 let cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight, cellNine, cellTen, cellEleven, cellTwelve, cellThirteen, cellFourteen, cellFifteen, cellSixteen, cellSeventeen, cellEighteen, cellNineteen;
 
 
 const hexagonArray = [
-    {cell : document.getElementById("cellOne")},
-    {cell : document.getElementById("cellTwo")},
-    {cell : document.getElementById("cellThree")},
-    {cell : document.getElementById("cellFour")},
-    {cell : document.getElementById("cellFive")},
-    {cell : document.getElementById("cellSix")},
-    {cell:  document.getElementById("cellSeven")},
-    {cell : document.getElementById("cellEight")},
-    {cell : document.getElementById("cellNine")},
-    {cell : document.getElementById("cellTen")},
-    {cell : document.getElementById("cellEleven")},
-    {cell : document.getElementById("cellTwelve")},
-    {cell : document.getElementById("cellThirteen")},
-    {cell : document.getElementById("cellFourteen")},
-    {cell : document.getElementById("cellFifteen")},
-    {cell : document.getElementById("cellSixteen")},
-    {cell : document.getElementById("cellSeventeen")},
-    {cell : document.getElementById("cellEighteen")},
-    {cell : document.getElementById("cellNineteen")}
+    {hexNumb : 1,     hexId : document.getElementById("cellOne")},
+    {hexNumb : 2,     hexId : document.getElementById("cellTwo")},
+    {hexNumb : 3,     hexId : document.getElementById("cellThree")},
+    {hexNumb : 4,     hexId : document.getElementById("cellFour")},
+    {hexNumb : 5,     hexId : document.getElementById("cellFive")},
+    {hexNumb : 6,     hexId : document.getElementById("cellSix")},
+    {hexNumb : 7,     hexId : document.getElementById("cellSeven")},
+    {hexNumb : 8,     hexId : document.getElementById("cellEight")},
+    {hexNumb : 9,     hexId : document.getElementById("cellNine")},
+    {hexNumb : 10,    hexId : document.getElementById("cellTen")},
+    {hexNumb : 11,    hexId : document.getElementById("cellEleven")},
+    {hexNumb : 12,    hexId : document.getElementById("cellTwelve")},
+    {hexNumb : 13,    hexId : document.getElementById("cellThirteen")},
+    {hexNumb : 14,    hexId : document.getElementById("cellFourteen")},
+    {hexNumb : 15,    hexId : document.getElementById("cellFifteen")},
+    {hexNumb : 16,    hexId : document.getElementById("cellSixteen")},
+    {hexNumb : 17,    hexId : document.getElementById("cellSeventeen")},
+    {hexNumb : 18,    hexId : document.getElementById("cellEighteen")},
+    {hexNumb : 19,    hexId : document.getElementById("cellNineteen")}
 ];
-const hexagonCells = hexagonArray.map((hexagoncell) => {
-    return hexagoncell.cell
-})
+
+function checkInitialArray(array) {
+    var currentIndex = array.length;
+     {
+  
+        // Pick a remaining element...
+       // randomIndex = Math.floor(Math.random() * currentIndex);
+    
+        // And swap it with the current element.
+        
+        array[currentIndex];
+      }
+    return array;
+
+};
+ checkInitialArray(hexagonArray);
+ console.log(hexagonArray);
+
+//creating the shuffled array for this game 
+//(must change on every game session)!
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  // Used like so
+  //var arr = [2, 11, 37, 42];
+  shuffle(hexagonArray);
+  console.log(hexagonArray);
+  console.log(document.getElementsByTagName("td")[0].style.backgroundColor = (hexagonArray[1].hexId));
 
 
-
-
-
-//document.getElementById("updates").open();
-//function writeFunction(){}
-//document.getElementsByTagName("textarea").getElementById("updates")};
-//document.getElementById("updates").write("hello world");
-//document.getElementById("updates").close;
 
 
 /*
@@ -72,38 +103,89 @@ for (let index = 0; index < array.length; index++) {
 
 
 
-//now we describe the random tile function
 
 
 
 
 
 //then we start the function
+
+////below is the random tile distributor module.
+//    let randomTile = hexagonArray[Math.floor(Math.random() * hexagonArray.length)];
+
+
+
+
+
+
+
+
+
+
+
+
+/*   waiting for hexArrayShuffled
+
+
+
+
+
+
 setInterval(() => {
+
+
+
+for(
+    //
+    let tileTypeRow = 0;
+    tileTypeRow < 19;
+    
+    tileTypeRow
+    ) 
+{ 
+*/
 for (
     // we want to sequentially work through all the tile types.
     let curPos = 0; 
-    // tileType[curPos]; curPos = 0 (aka sheep);
-    curPos < 6; //
+    
+    // tileType[curPos]; curPos = 0 (from the color sheep);
+    //curPos = 0; sheep 
+    //curPos = 1; wood
+    //curPos = 2; wheat
+    //curPos = 3; ore
+    //curPos = 4; brick
+    //curPos = 5; desert
+    
+    curPos >= 6; //
     //every loop we increment the current position
     //and thus we work through the array
     curPos++) 
     {
         
+        
     //set iterator variable to 1 (0 in js)
 //for each loop we check the 
-let randomTile = remainingTiles[Math.floor(Math.random() * remainingTiles.length)];
+//let randomTile = remainingTiles[Math.floor(Math.random() * remainingTiles.length)];
 
 //console.log(`dealing ${remainingTiles}`)
 let remaining = --remainingTiles[curPos].amountTiles;
 
-if (remaining <= 0) { break; }
+if (remaining <= 0) 
+{ break; 
+} else {
 //remainingTiles[0].amountTiles = return amountTiles on first row.
-console.log(`There are ${remaining} remaining ${tileType[0]}: `);
 
-}
+console.log(`There are ${remaining} remaining ${tileType[0]}`);
 
-}, 2000)
+//console.log(randomTile);
+//document.getElementsByTagName(randomTile)[curPos].style.backgroundColor = (remainingTiles[0].sheepTile)
+
+} //else
+} //curPos end for loop
+/*}//tileTypeRow loop
+
+}, 2000) 
+
 
 
 
@@ -133,7 +215,7 @@ for (
 
         // var arr = [10, 1, 2, 3].shift(); 
         //console.log("Shifted value is : " + arr ) 
-       //document.getElementsByTagName("td")[curPos].style.backgroundColor = ("red")
+       document.getElementsByTagName("td")[curPos].style.backgroundColor = ("red")
 
         //console.log(remainingTiles[curPos].amountTiles)
 // if there are tiles left the console returns true
@@ -187,10 +269,7 @@ while
   i++
 }
 */
-//below is the random tile distributor module.
-/*let randomTile = ninteenTiles[Math.floor(Math.random() * nineteenTiles.length)];
-console.log(randomTile);
-*/
+
 
 //function colorFunction (backup)
 /*function colorFunction() {
