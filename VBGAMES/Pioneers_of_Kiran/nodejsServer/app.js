43f72841b6bfdeac6e8edb5200e4feedddabe0fb
kiren77webdev <kiren77webdev@users.noreply.github.com>
2021-01-26 16:14:10 +1100

    yes

--- VBGAMES/Pioneers_of_Kiran/nodejsServer/app.js
+++ VBGAMES/Pioneers_of_Kiran/nodejsServer/app.js
@@ -1,57 +1,28 @@
-//include http, fs and url module
-var http = require("http"),
-  fs = require("fs"),
-  path = require("path"),
-  url = require("url");
-imageDir = "/Users/webdevkiren/Web Dev/kiren77.github.io/VBGAMES/Pioneers_of_Kiran/gameAssets/x/";
+const http = require('http')
+const fs = require('fs') 
+//library that has all the file handling capabilities
+const port = 3000
 
-//create http server listening on port 3333
-http
-  .createServer(function (req, res) {
-    //use the url to parse the requested url and get the image name
-    var query = url.parse(req.url, true).query;
-    pic = query.image;
 
-    if (typeof pic === "undefined") {
-      getImages(imageDir, function (err, files) {
-        var imageLists = "<ul>";
-        for (var i = 0; i < files.length; i++) {
-          imageLists +=
-            '<li><a href="/?image=' + files[i] + '">' + files[i] + "</li>";
-        }
-        imageLists += "</ul>";
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.end(imageLists);
-      });
-    } else {
-      //read the image using fs and send the image content back in the response
-      fs.readFile(imageDir + pic, function (err, content) {
-        if (err) {
-          res.writeHead(400, { "Content-type": "text/html" });
-          console.log(err);
-          res.end("No such image");
-        } else {
-          //specify the content type in the response will be an image
-          res.writeHead(200, { "Content-type": "image/jpg" });
-          res.end(content);
-        }
-      });
-    }
+const server = http.createServer(function(req, res) {
+    //200 is status code that means that the page rendered properly 
+    //(e.g. as opposed to 404)
+    res.writeHead(200, { 'Content-Type': 'text/html'})
+    fs.readFile('index.html', function(error, data){
+      if (error) {
+       res.writeHead(404)
+    res.write('Error: File not found!')
+   } else {
+       res.write(data) //data is all the info inside the html
+   }
+   res.end()
   })
-  .listen(3333);
-console.log("Server running at http://localhost:3333/");
+})
 
-//get the list of jpg files in the image dir
-function getImages(imageDir, callback) {
-  var fileType = ".jpg",
-    files = [],
-    i;
-  fs.readdir(imageDir, function (err, list) {
-    for (i = 0; i < list.length; i++) {
-      if (path.extname(list[i]) === fileType) {
-        files.push(list[i]); //store the file name into the array files
-      }
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something went wrong', error)
+    } else {
+        console.log('Server is listening on port ' + port)
     }
-    callback(err, files);
-  });
-}
+})
