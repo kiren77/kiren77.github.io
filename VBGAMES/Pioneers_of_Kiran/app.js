05eff1175d530011acb92a5060b097d914f99349
kiren77webdev <kiren77webdev@users.noreply.github.com>
2021-01-26 17:39:19 +1100

    p

--- VBGAMES/Pioneers_of_Kiran/app.js
+++ VBGAMES/Pioneers_of_Kiran/app.js
@@ -1,28 +1,37 @@
-const http = require('http')
-const fs = require('fs') 
-//library that has all the file handling capabilities
-const port = 3000
+"use strict";
+const express = require("express");
+const app = express();
+const port = process.env.port || 5005;
+// https://expressjs.com/en/4x/api.html#express.static
 
+let options = {
+  dotfiles: "ignore", //allow, deny, ignore
+  etag: true,
+  extensions: ["htm", "html"],
+  index: false, //to disable directory indexing
+  maxAge: "7d",
+  redirect: false,
+  setHeaders: function(res, path, stat) {
+    //add this header to all static responses
+    res.set("x-timestamp", Date.now());
+  }
+};
 
-const server = http.createServer(function(req, res) {
-    //200 is status code that means that the page rendered properly 
-    //(e.g. as opposed to 404)
-    res.writeHead(200, { 'Content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data){
-      if (error) {
-       res.writeHead(404)
-    res.write('Error: File not found!')
-   } else {
-       res.write(data) //data is all the info inside the html
-   }
-   res.end()
-  })
-})
+app.use(express.static("public", options));
+//you can use https://favicon.io/favicon-generator/ to create the favicon.ico
 
-server.listen(port, function(error) {
-    if (error) {
-        console.log('Something went wrong', error)
-    } else {
-        console.log('Server is listening on port ' + port)
-    }
-})
+app.get("/", (req, res) => {
+  let img = `<img src="/img/cotton-candy.gif"/>`;
+  let secret = `<a href="/.htaccess">secret</a>`;
+
+  let html = `<!Doctype html><html><head><title>Sample</title></head>`;
+  html += `<body><h1>Sample HTML</h1><main>${secret}</main></body></html>`;
+  res.send(html);
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.log("ERROR", err);
+  }
+  console.log(`Listening on port ${port}`);
+});
