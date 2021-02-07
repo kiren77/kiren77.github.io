6fbbb99e7122757721b5b14bc025eccd72b962aa
kiren77 <kiren77@users.noreply.github.com>
2021-02-07 18:33:49 +1100

    more placeholder tweaking

---
+++ VBGAMES/Pioneers_of_Kiran/revealShuffledTiles.js
@@ -0,0 +1,8 @@
+
+
+function showStuff(id) {
+    document.getElementById(id).style.visibility = "hidden";
+    // hide the lorem ipsum text
+    document.getElementById(id).style.visibility = "visible";
+    // hide the link
+}
